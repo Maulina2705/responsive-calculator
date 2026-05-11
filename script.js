aaf665ec7b9947179329d7421baaf257aee5cb9e
Maulina Hilwa Salsabillah <112769653+Maulina2705@users.noreply.github.com>
2026-05-11 09:54:34 +0700

    Scientific Function Logic

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const clearButton = document.querySelector(".clear");
 const backspaceButton = document.querySelector(".backspace");
 
 const equalButton = document.querySelector(".equal");
+const scientificButtons = document.querySelectorAll(".scientific");
 
 /* =========================
    NUMBER BUTTON
@@ -141,5 +142,102 @@ document.addEventListener("keydown", (event) => {
         screen.value = "";
 
     }
+
+});
+
+/* =========================
+   SCIENTIFIC BUTTONS
+========================= */
+
+scientificButtons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        const value = button.textContent;
+
+        switch(value){
+
+            case "sin":
+                screen.value = Math.sin(eval(screen.value));
+                break;
+
+            case "cos":
+                screen.value = Math.cos(eval(screen.value));
+                break;
+
+            case "tan":
+                screen.value = Math.tan(eval(screen.value));
+                break;
+
+            case "asin":
+                screen.value = Math.asin(eval(screen.value));
+                break;
+
+            case "acos":
+                screen.value = Math.acos(eval(screen.value));
+                break;
+
+            case "atan":
+                screen.value = Math.atan(eval(screen.value));
+                break;
+
+            case "log":
+                screen.value = Math.log10(eval(screen.value));
+                break;
+
+            case "ln":
+                screen.value = Math.log(eval(screen.value));
+                break;
+
+            case "π":
+                screen.value += Math.PI;
+                break;
+
+            case "e":
+                screen.value += Math.E;
+                break;
+
+            case "√":
+                screen.value = Math.sqrt(eval(screen.value));
+                break;
+
+            case "x²":
+                screen.value = Math.pow(eval(screen.value), 2);
+                break;
+
+            case "x³":
+                screen.value = Math.pow(eval(screen.value), 3);
+                break;
+
+            case "^":
+                screen.value += "**";
+                break;
+
+            case "(":
+                screen.value += "(";
+                break;
+
+            case ")":
+                screen.value += ")";
+                break;
+
+            case "|x|":
+                screen.value = Math.abs(eval(screen.value));
+                break;
+
+            case "1/x":
+                screen.value = 1 / eval(screen.value);
+                break;
+
+            case "EXP":
+                screen.value += "e";
+                break;
+
+            default:
+                break;
+
+        }
+
+    });
 
 });
