e3907bffe1e2c9f411eaa912bffe28e82539fbff
Maulina Hilwa Salsabillah <112769653+Maulina2705@users.noreply.github.com>
2026-05-11 20:05:37 +0700

    cape banget

--- script.js
+++ script.js
@@ -480,7 +480,13 @@ function renderFraction(fraction) {
 numberButtons.forEach(button => {
 
     button.addEventListener("click", () => {
-        showingFraction = false;
+        if (showingFraction) {
+
+            showingFraction = false;
+
+            updateScreen("0");
+
+        }
 
         if (waitingForNewNumber) {
 
@@ -515,6 +521,12 @@ numberButtons.forEach(button => {
 ========================= */
 
 operatorButtons.forEach(button => {
+
+    if (showingFraction) {
+
+        showingFraction = false;
+
+    }
 
     button.addEventListener("click", () => {
 
@@ -570,6 +582,7 @@ operatorButtons.forEach(button => {
 
 clearButton.addEventListener("click", () => {
 
+    showingFraction = false;
     updateScreen("0");
 
     expressionDisplay.textContent = "";
@@ -581,6 +594,8 @@ clearButton.addEventListener("click", () => {
 ========================= */
 
 backspaceButton.addEventListener("click", () => {
+
+    showingFraction = false;
 
     expressionDisplay.textContent =
         expressionDisplay.textContent
@@ -1076,19 +1091,25 @@ scientificButtons.forEach(button => {
 
                 screen.innerHTML = `
 
-        <div class="fraction">
+    <div class="fraction">
+
+        <span class="whole"></span>
+
+        <div class="fraction-stack">
 
             <span class="top">
-                1
+                ${parts[0]}
             </span>
 
             <span class="bottom">
-                2
+                ${parts[1]}
             </span>
 
         </div>
 
-    `;
+    </div>
+
+`;
 
                 break;
 
